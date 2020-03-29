24eb7961abb08d9f14d38dc127aa52929fe01143
Anthon_M <xll2005@tut.by>
2020-03-29 12:52:30 +0300

    feat: implement caesar cipher

---
+++ caesar_cli/src/caesarCipher.js
@@ -0,0 +1,25 @@
+const caesarCipher = (cipherAlphabet, string, action, shift) => {
+  const { length } = cipherAlphabet;
+  const cipherShift = Number(action === 'encode' ? shift : -shift);
+
+  const getEncodeChar = char => {
+    const loverChar = char.toLowerCase();
+    const isUpper = char !== loverChar;
+    const index = cipherAlphabet.indexOf(loverChar);
+
+    if (index === -1) {
+      return char;
+    }
+    const cipherCharIndex = (index + cipherShift + length) % length;
+    return isUpper
+      ? cipherAlphabet[cipherCharIndex].toUpperCase()
+      : cipherAlphabet[cipherCharIndex];
+  };
+
+  return string
+    .split('')
+    .map(getEncodeChar)
+    .join('');
+};
+
+exports.caesarCipher = caesarCipher;
